af9c4a00b09de8a4ee0381f3faa993f66ac18cd2
lediep925 <132372205+lediep925@users.noreply.github.com>
2023-09-11 22:52:24 +0700

    Add files via upload

---
+++ demo3/bài 3/bai3p1.php
@@ -0,0 +1,29 @@
+<DOCTYPE html PUBLIC"-W3c//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title>Xuất số thành chữ</title>
+</head>
+
+<body>
+<form action="bai3p1.php" method="POST">
+<table width="519" border="1">
+<tr>
+<td colspan="3">Đọc số</td>
+</tr>
+<tr>
+<td>Nhập số(0-9)</td>
+<td width="69" rowspan="2"><input type="submit" name="button" id="button"
+value="submit"/></td>
+<td> Bằng chữ</td>
+</tr>
+<tr>
+<td width="177"><p>
+<label for="textfield"></label>
+<input type="text" name="so" id="textfield2"/></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
